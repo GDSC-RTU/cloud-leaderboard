95be9b43de919d2e08c19ea3a14b504e0af00e35
ishubham21 <sg2199203@gmail.com>
2021-10-13 20:11:56 +0530

    enabled import pipe

---
+++ csv2json.js
@@ -0,0 +1,18 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+fs.createReadStream('csv/data.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    fs.writeFileSync(
+      "src/data.json",
+      JSON.stringify(results),
+      (err) => {
+        if (err) throw err;
+        console.log("Data file has been saved!");
+      }
+    );
+  });
+
--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 
 function App() {
+
+  const data = require('./../src/data.json')
+  console.log(data);
+  
   return (
     <div className="App">
+
     </div>
   );
 }
