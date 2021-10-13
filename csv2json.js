49b08b531fbe67fed54699d7d40375f25ea3f673
ishubham21 <sg2199203@gmail.com>
2021-10-13 22:00:14 +0530

    sorted the data and filtered it

--- csv2json.js
+++ csv2json.js
@@ -5,7 +5,19 @@ const results = [];
 fs.createReadStream('csv/data.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
-  .on('end', () => {
+  .on('end', () => {    //to create synchronicity - i.e. after the data is recieved
+    
+    //calculating the number of labs and appending it in the results
+    results.forEach((result) => {
+      let totalLabs = parseInt(result['# of Skill Badges Completed in Track 1']) + parseInt(result['# of Skill Badges Completed in Track 2'])
+      result.totalLabs = totalLabs
+    })
+    
+    //descending sort on the basis of total number of labs completed
+    //if labs are same, sort on the basis of Student Name 
+    results.sort((a, b) => (b.totalLabs - a.totalLabs) || a["Student Name"] - b["Student Name"])
+
+    //creating a file called participantData
     fs.writeFileSync(
       "src/participantData.json",
       JSON.stringify(results),
