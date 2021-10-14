27042469519b126b46bb3c31a4064434d024e208
ishubham21 <sg2199203@gmail.com>
2021-10-14 20:34:25 +0530

    styling fixed + meta tags added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Leaderboard from './components/Leaderboard'
 
 function App() {
 
+  //importing the json data from produced file
   const data = require('./../src/participantData.json')
-  console.log(data);
 
   return (
     <div className="App">
@@ -12,6 +12,10 @@ function App() {
         <h1>GDSC RTU Google Cloud Program Leaderboard</h1>
       </div>
       <Leaderboard participantData={data}/>
+
+      <div className="team">
+        Made with ❤️ by <a href="https://gdsc.community.dev/rajasthan-technical-university-kota/">Google DSC RTU</a>
+      </div>
     </div>
   );
 
